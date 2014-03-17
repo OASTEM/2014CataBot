7b2875921ef2d39bde2f8ce8eef14d9cf98faaa7
Kevin Tran <kevin@oastem.org>
2014-03-17 01:29:17 -0700

    Changed fireLim checking for thread concurrency.

---
+++ src/org/oastem/frc/imaging/Scores.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oastem.frc.imaging;
+
+/**
+ *
+ * @author KTOmega
+ */
+public class Scores {
+    public double rectangularity;
+    public double aspectRatioInner;
+    public double aspectRatioOuter;
+    public double xEdge;
+    public double yEdge;
+}
